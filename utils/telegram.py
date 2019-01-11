b0acdaa2518998bfe99519ad1c4bf663250066cc
fakedon <afterlot@hotmail.com>
2019-01-12 02:44:50 +0800

    telethon change to Telethon-sync

--- utils/telegram.py
+++ utils/telegram.py
@@ -19,37 +19,32 @@ def get_tg(cfg=None, session=None, api_id=None, api_hash=None, phone=None, proxy
 
     if session:
         config['session'] = session
-    else:
-        return None
     if api_id:
         config['api_id'] = api_id
-    else:
-        return None
     if api_hash:
         config['api_hash'] = api_hash
-    else:
-        return None
     if phone:
         config['phone'] = phone
-    else:
-        return None
     if proxy:
         config['proxy'] = proxy
     else:
         config['proxy'] = config.get('proxy', None)
 
-    client = TelegramClient(config['session'], config['api_id'], config['api_hash'], proxy=config['proxy'])
-    client.connect()
+    try:
+        client = TelegramClient(config['session'], config['api_id'], config['api_hash'], proxy=config['proxy'])
+        client.connect()
 
-    if not client.is_user_authorized():
-        # client.send_code_request(config['phone'])
-        # client.sign_in(config['phone'], input('Enter the code: '))
-        client.sign_in(phone=config['phone'])
-        client.sign_in(code=input('Enter the code: '))
+        if not client.is_user_authorized():
+            # client.send_code_request(config['phone'])
+            # client.sign_in(config['phone'], input('Enter the code: '))
+            client.sign_in(phone=config['phone'])
+            client.sign_in(code=input('Enter the code: '))
 
-    return client
+        return client
+    except Exception:
+        return None
 
 
 if __name__ == '__main__':
-    c = get_tg_client('ameb')
+    c = get_tg()
     print(c.get_me())
